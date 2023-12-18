ac9e43ee179e7e64a92a6985c228cf51a3604c91
ShivamRanjan069 <shivambth069@gmail.com>
2023-12-18 15:08:10 +0530

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import styles from "../Navbar.module.css";
+import { getImageUrl } from "../../utils";
+
+const Navbar = () => {
+  const [menuOpen, setmenuOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        SHIVAM
+      </a>
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={() => setmenuOpen(!menuOpen)}
+        />
+         <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setmenuOpen(false)}
+        >
+          <li>
+            <a href="/About">About</a>
+          </li>
+          <li>
+            <a href="/Experience">Skills</a>
+          </li>
+          <li>
+            <a href="/Contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
